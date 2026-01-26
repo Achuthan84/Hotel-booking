3753f6a62e0ea64b358ff1d478dea2902d4a11f9
Achuthan84 <achuthan302004@gmail.com>
2026-01-26 16:52:26 +0530

    commit

--- src/Pages/AllRooms.jsx
+++ src/Pages/AllRooms.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets, roomsDummyData } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+import StarRating from '../components/StarRating'
 
 const AllRooms = () => {
+    const navigate = useNavigate();
     return (
-        <div>AllRooms</div>
+        <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 nd:px-16 lg:px-24 xl:px-32'>
+            <div>
+                <div className='flex flex-col items-start text-left'>
+                    <h1 className='font-playfair text-4xl md:text-[40px]'>Hotel Rooms</h1>
+                    <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories.</p>
+                </div>
+            </div>
+            {roomsDummyData.map((room) => (
+                <div>
+                    <img onClick={() => { navigate(`/rooms/${room._id}`); scrollTo(0, 0) }}
+                        src={room.images[0]} alt="hotel-img" title='View Rooms Details' className='max-h-65 md:w-1/2
+                         rounded-full shadow-lg object-cover cursor-pointer' />
+                    <div className='md:w-1/2 flex flex-col gap-2'>
+                        <p className='text-gray-500'>{room.hotel.city}</p>
+                        <p onClick={() => { navigate(`/rooms/${room._id}`); scrollTo(0, 0) }} className='text-gray-800 text-3xl font-playfair cursor-pointer'>{room.hotel.name}</p>
+                        <div className='flex items-center'>
+                            <StarRating />
+                            <p className='ml-2'>200+ Reviews</p>
+                        </div>
+                        <div className='flex items-center gap-1 text-gray-500 mt-2 text-sm'>
+                            <img src={assets.locationIcon} alt="location-icon" />
+                            <span>{room.hotel.address}</span>
+                        </div>
+                    </div>
+                </div>
+            ))}
+
+            <div>
+
+            </div>
+        </div>
     )
 }
 
