6328034930fd166a2d3d350459dd4661e97a2d09
achuthan.ks <absachuthanks@gmail.com>
2026-03-18 18:58:20 +0530

    owner

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AllRooms from './Pages/AllRooms';
 import RoomDetails from './Pages/RoomDetails';
 import MyBookings from './Pages/MyBookings';
 import HotelReg from './components/HotelReg';
+import Layout from './Pages/HotelOwner/Layout';
 
 const App = () => {
 
@@ -22,6 +23,9 @@ const App = () => {
           <Route path='/rooms' element={<AllRooms />} />
           <Route path='/rooms/:id' element={<RoomDetails />} />
           <Route path='/my-bookings' element={<MyBookings />} />
+          <Route path='/owner' element={<Layout/>}>
+
+          </Route>
         </Routes>
       </div>
       {!isOwnerPath && <Footer />}
